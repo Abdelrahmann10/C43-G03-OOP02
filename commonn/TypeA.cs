9bfcb0e46e3e3113e069801b663d280290788fb3
Abdelrahmann10 <abdosorour10@gmail.com>
2024-12-24 22:40:06 +0200

    Add files via upload

---
+++ commonn/TypeA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commonn
+{
+    public class TypeA
+    {
+        protected int a;
+        private protected int x;    //Private      //Can inherit    
+        protected int y;            //Private      //Can inherit but can't use
+        internal protected int z;   //Internal     //Can inherit inside the same project  
+    }
+}
---
+++ commonn/TypeB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commonn
+{
+    public class TypeB : TypeA
+    {
+        void fun01()
+        {
+            a = 12; //  private 
+            x = 12; //  internal
+            y = 12; //  private   
+
+            TypeA typeA = new TypeA();
+            typeA.z = 12;
+
+            TypeB typeB = new TypeB();
+            typeB.x = 12;
+            typeB.y = 12;
+            typeB.z = 12;
+
+        }
+    }
+
+    class TypeC
+    {
+        void Fun01()
+        {
+            TypeB typeB = new TypeB();
+            typeB.z = 20;
+        }
+    }
+}
